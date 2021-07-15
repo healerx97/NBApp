388f7922335671e25dfcb9592d6214bb4bddf1d8
Kenny Yoon <ghun.yoon@gmail.com>
2021-07-15 18:56:08 -0400

    teams

--- src/Components/LoadTeam.js
+++ src/Components/LoadTeam.js
@@ -1,11 +1,11 @@
-
+import { Card } from 'react-bootstrap'
 function LoadTeam({ team }) {
     let abb = team.tricode.toLowerCase()
     return (
-        <div>
-            <h3>Team Name: {team.team_name}</h3>
-            <img src={`https://www.nba.com/.element/img/1.0/teamsites/logos/teamlogos_500x500/${abb}.png`} alt="image"/>
-        </div>
+        <Card style={{ width: '15rem' }} className="box">
+            <Card.Img variant="top" src={`https://www.nba.com/.element/img/1.0/teamsites/logos/teamlogos_500x500/${abb}.png`} alt="image" />
+            <Card.Title>{team.team_name}</Card.Title>
+        </Card>
     )
 }
 
--- src/Components/Teams.js
+++ src/Components/Teams.js
@@ -1,6 +1,15 @@
 import LoadTeam from "./LoadTeam"
+import { CardDeck,Row,Col} from 'react-bootstrap'
+
 function Teams({teamData}) {
-    const renderTeams = teamData.map(team => <LoadTeam team = {team} key = {team.id}/>)
+    const renderTeams = (<Row xs={1} md={5} className="g-4">
+    {Array.from(teamData).map((team, idx) => (
+        <Col>
+        <LoadTeam team = {team} key = {team.id}/>
+        </Col>
+    ))}
+    </Row>)
+    
     return (
         <div>
             {renderTeams}
